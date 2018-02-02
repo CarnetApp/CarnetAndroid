819a938a59e1205cecfc875b16290966a41fc0ff
Phie <phie@phie.ovh>
2018-02-02 18:41:58 +0100

    sync intro

--- app/src/main/java/com/spisoft/quicknote/synchro/HelpActivity.java
+++ app/src/main/java/com/spisoft/quicknote/synchro/HelpActivity.java
@@ -4,6 +4,10 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
@@ -14,11 +18,22 @@ public class HelpActivity extends AppCompatActivity {
 
     private static final String SHOULD_START_ACTIVITY = "should_start_gdrive_act";
 
+    private static final int NUM_PAGES = 5;
+
+    private ViewPager mPager;
+    private ScreenSlidePagerAdapter mPagerAdapter;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
         PreferenceManager.getDefaultSharedPreferences(this).edit().putBoolean(SHOULD_START_ACTIVITY, false).commit();
+        // Instantiate a ViewPager and a PagerAdapter.
+        mPager = (ViewPager) findViewById(R.id.pager);
+        mPagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
+        mPager.setAdapter(mPagerAdapter);
+
     }
     public void addAccount(View v){
         startActivity(new Intent(this,AuthorizeActivity.class));
@@ -28,7 +43,23 @@ public class HelpActivity extends AppCompatActivity {
         finish();
     }
     public static boolean shouldStartActivity(Context context){
-        return PreferenceManager.getDefaultSharedPreferences(context).getBoolean(SHOULD_START_ACTIVITY, true);
+        return PreferenceManager.getDefaultSharedPreferences(context).getBoolean(SHOULD_START_ACTIVITY, true)||true;
     }
 
+    private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
+        public ScreenSlidePagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return new SynchroIntroductionFragment();
+        }
+
+        @Override
+        public int getCount() {
+            return NUM_PAGES;
+        }
+    }
 }
+
---
+++ app/src/main/java/com/spisoft/quicknote/synchro/SynchroIntroductionFragment.java
@@ -0,0 +1,34 @@
+package com.spisoft.quicknote.synchro;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.spisoft.quicknote.R;
+
+
+public class SynchroIntroductionFragment extends Fragment {
+
+
+    public SynchroIntroductionFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_synchro_introduction, container, false);
+    }
+
+}
