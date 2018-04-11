d887d413b76e3b71fa69efa7a3b47b4b95f641c0
Phie <phie@phie.ovh>
2018-04-11 18:38:46 +0200

    remove/readd sync folder + restart app after changing root folder

--- app/src/main/java/com/spisoft/quicknote/StorageDialog.java
+++ app/src/main/java/com/spisoft/quicknote/StorageDialog.java
@@ -1,10 +1,16 @@
 package com.spisoft.quicknote;
 
+import android.app.AlarmManager;
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.Handler;
 import android.support.v4.app.DialogFragment;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -15,7 +21,10 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.spisoft.quicknote.synchro.googledrive.DBDriveFileHelper;
+import com.spisoft.sync.account.DBAccountHelper;
+import com.spisoft.sync.utils.Utils;
+import com.spisoft.sync.wrappers.Wrapper;
+import com.spisoft.sync.wrappers.WrapperFactory;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -30,11 +39,11 @@ public class StorageDialog extends DialogFragment implements DialogInterface.OnC
     private ListAdapter mListAdapter;
     private File mFile;
     private View mParentFolderButton;
-
+    private Handler mHandler;
 
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         mListAdapter = new ListAdapter();
-
+        mHandler = new Handler();
         List<File> files = new ArrayList<>();
         files.add(new File(PreferenceHelper.getRootPath(getActivity())));
         //files.add(new File(getContext().get));
@@ -62,9 +71,38 @@ public class StorageDialog extends DialogFragment implements DialogInterface.OnC
         builder.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                Toast.makeText(getContext(), "ok !" + mFile, Toast.LENGTH_LONG).show();
-            PreferenceHelper.setRootPath(getActivity(), mFile.getAbsolutePath());
-                DBDriveFileHelper.getInstance(getActivity()).deleteAll();
+                /*
+                    update sync
+                 */
+                Cursor accountCursor = DBAccountHelper.getInstance(getContext()).getCursor();
+                if(accountCursor!=null && accountCursor.getCount()>0){
+                    int typeCol =accountCursor.getColumnIndex(DBAccountHelper.KEY_ACCOUNT_TYPE);
+                    int idCol = accountCursor.getColumnIndex(DBAccountHelper.KEY_ACCOUNT_ID);
+                    accountCursor.moveToFirst();
+                    do{
+                        Wrapper wrapper = WrapperFactory.getWrapper(getActivity(),accountCursor.getInt(typeCol) , accountCursor.getInt(idCol));
+                        String remote = wrapper.getRemoteSyncDir(PreferenceHelper.getRootPath(getContext()));
+                        wrapper.removeSyncDir(PreferenceHelper.getRootPath(getContext()));
+                        wrapper.addFolderSync(mFile.getAbsolutePath(), remote);
+
+                    }while(accountCursor.moveToNext());
+                }
+                PreferenceHelper.setRootPath(getActivity(), mFile.getAbsolutePath());
+                Toast.makeText(getContext(), R.string.app_restart, Toast.LENGTH_LONG).show();
+
+                mHandler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        Intent mStartActivity = new Intent(Utils.context, MainActivity.class);
+                        int mPendingIntentId = 123456;
+                        PendingIntent mPendingIntent = PendingIntent.getActivity(Utils.context, mPendingIntentId,    mStartActivity, PendingIntent.FLAG_CANCEL_CURRENT);
+                        AlarmManager mgr = (AlarmManager)Utils.context.getSystemService(Context.ALARM_SERVICE);
+                        mgr.set(AlarmManager.RTC, System.currentTimeMillis() + 100, mPendingIntent);
+                        System.exit(0);
+                    }
+                },1000);
+
+
             }
         });
         builder.setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
