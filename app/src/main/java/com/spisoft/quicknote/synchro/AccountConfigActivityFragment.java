0cf9cee3412ac11c25508a7f7338d58239744846
Phie <phie@phie.ovh>
2019-01-24 18:01:46 +0100

    options to delete account / change credentials

--- app/src/main/java/com/spisoft/quicknote/synchro/AccountConfigActivityFragment.java
+++ app/src/main/java/com/spisoft/quicknote/synchro/AccountConfigActivityFragment.java
@@ -1,20 +1,18 @@
 package com.spisoft.quicknote.synchro;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 import com.spisoft.quicknote.PreferenceHelper;
 import com.spisoft.quicknote.R;
 import com.spisoft.sync.Log;
+import com.spisoft.sync.account.DBAccountHelper;
 import com.spisoft.sync.browsing.FilePickerActivity;
-import com.spisoft.sync.wrappers.*;
 import com.spisoft.sync.wrappers.Wrapper;
 
 /**
@@ -29,6 +27,8 @@ public class AccountConfigActivityFragment extends PreferenceFragment implements
     private int mAccountType;
     private Wrapper mWrapper;
     private String mCurrentlySetPath;
+    private Preference mChangeCredentialsPreference;
+    private Preference mDeleteAccountPreference;
 
     public AccountConfigActivityFragment() {
     }
@@ -47,6 +47,16 @@ public class AccountConfigActivityFragment extends PreferenceFragment implements
         mBrowsePreference.setSummary(mCurrentlySetPath);
         mBrowsePreference.setOnPreferenceClickListener(this);
         getPreferenceScreen().addPreference(mBrowsePreference);
+        if(mWrapper.canChangeCredentials()){
+            mChangeCredentialsPreference = new Preference(getActivity());
+            mChangeCredentialsPreference.setTitle(R.string.change_credentials);
+            mChangeCredentialsPreference.setOnPreferenceClickListener(this);
+            getPreferenceScreen().addPreference(mChangeCredentialsPreference);
+        }
+        mDeleteAccountPreference = new Preference(getActivity());
+        mDeleteAccountPreference.setTitle(R.string.delete);
+        mDeleteAccountPreference.setOnPreferenceClickListener(this);
+        getPreferenceScreen().addPreference(mDeleteAccountPreference);
     }
 
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -62,12 +72,28 @@ public class AccountConfigActivityFragment extends PreferenceFragment implements
     }
     @Override
     public boolean onPreferenceClick(Preference preference) {
-        Intent intent = new Intent(getActivity(), FilePickerActivity.class);
-        intent.putExtra(FilePickerActivity.EXTRA_ACCOUNT_ID, mAccountId);
-        intent.putExtra(FilePickerActivity.EXTRA_START_PATH, mCurrentlySetPath);
-        intent.putExtra(FilePickerActivity.EXTRA_AS_FILE_PICKER, true);
-        intent.putExtra(FilePickerActivity.EXTRA_DISPLAY_ONLY_MIMETYPE, "DIR");
-        startActivityForResult(intent,REQUEST_FILE_PICK);
+        if (preference == mBrowsePreference) {
+            Intent intent = new Intent(getActivity(), FilePickerActivity.class);
+            intent.putExtra(FilePickerActivity.EXTRA_ACCOUNT_ID, mAccountId);
+            intent.putExtra(FilePickerActivity.EXTRA_START_PATH, mCurrentlySetPath);
+            intent.putExtra(FilePickerActivity.EXTRA_AS_FILE_PICKER, true);
+            intent.putExtra(FilePickerActivity.EXTRA_DISPLAY_ONLY_MIMETYPE, "DIR");
+            startActivityForResult(intent, REQUEST_FILE_PICK);
+        } else if (preference == mChangeCredentialsPreference){
+            mWrapper.startAuthorizeActivityForResult(getActivity(), 0);
+        } else if (preference == mDeleteAccountPreference){
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            builder.setMessage(R.string.delete_confirm);
+            builder.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    mWrapper.deleteAccount();
+                    getActivity().finish();
+                }
+            });
+            builder.setNegativeButton(android.R.string.cancel, null);
+            builder.show();
+        }
         return true;
     }
 }
