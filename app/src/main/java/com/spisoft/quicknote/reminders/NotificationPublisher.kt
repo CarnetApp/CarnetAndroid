66fa5d59856a9186ae9cd25e4602bf352fdccd85
Phie <phie@phie.ovh>
2019-05-24 22:44:30 +0200

    readd note reminders after notifying

--- app/src/main/java/com/spisoft/quicknote/reminders/NotificationPublisher.kt
+++ app/src/main/java/com/spisoft/quicknote/reminders/NotificationPublisher.kt
@@ -6,15 +6,24 @@ import android.app.NotificationManager
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import com.spisoft.quicknote.databases.RemindersManager
+import com.spisoft.sync.Log
 
 class NotificationPublisher : BroadcastReceiver() {
-
+    val TAG = "NotificationPublisher"
     override fun onReceive(context: Context, intent: Intent) {
 
         val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         val notification = intent.getParcelableExtra<Notification>(NOTIFICATION)
-        val id = intent.getIntExtra(NOTIFICATION_ID, 0)
-        notificationManager.notify(id, notification)
+        val path = intent.getStringExtra(NOTE_PATH)
+        val id = intent.getIntExtra(NOTIFICATION_ID, 4)
+        if(notification != null)
+            notificationManager.notify(id, notification)
+        else
+            RemindersManager.getInstance(context)!!.onNotified(path)
+
+        Log.d(TAG, "notification "+(notification == null))
+        Log.d(TAG, "notification id "+id)
 
     }
 
@@ -22,5 +31,6 @@ class NotificationPublisher : BroadcastReceiver() {
 
         var NOTIFICATION_ID = "notification-id"
         var NOTIFICATION = "notification"
+        var NOTE_PATH = "note_path"
     }
 }
