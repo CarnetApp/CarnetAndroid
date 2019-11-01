4e917222407aa8c9ea487791e0c70c6015b49664
Phie <phie@phie.ovh>
2019-11-01 13:46:50 +0100

    start sync on boot (when needed)

--- app/src/main/java/com/spisoft/quicknote/BootReceiver.kt
+++ app/src/main/java/com/spisoft/quicknote/BootReceiver.kt
@@ -4,11 +4,13 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import com.spisoft.quicknote.reminders.RemindersManager
+import com.spisoft.sync.synchro.SynchroService
 
 class BootReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
         // This method is called when the BroadcastReceiver is receiving an Intent broadcast.
         RemindersManager.getInstance(context)!!.onBoot()
+        SynchroService.startOnBootIfNeeded(context)
     }
 }
