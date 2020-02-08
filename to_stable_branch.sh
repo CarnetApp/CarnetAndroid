9d6b7631bdeb6f84c5cffb577acd4ade70fb65c2
Phie <phie@phie.ovh>
2020-02-08 22:34:48 +0100

    update release script

--- to_stable_branch.sh
+++ to_stable_branch.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 
+#git checkout stable
+#git checkout -b stable-$(grep -o "versionName\s\+.*" app/build.gradle | tail -1 | awk '{ print $2 }' | tr -d \''"\')
+#git push origin stable-$(grep -o "versionName\s\+.*" app/build.gradle | tail -1 | awk '{ print $2 }' | tr -d \''"\')
+
 git branch -d stable
 git push origin --delete stable
 git checkout master
 git checkout -b stable
 git push origin stable
+git checkout -b stable-$(grep -o "versionName\s\+.*" app/build.gradle | tail -1 | awk '{ print $2 }' | tr -d \''"\')
+git push origin stable-$(grep -o "versionName\s\+.*" app/build.gradle | tail -1 | awk '{ print $2 }' | tr -d \''"\')
 git checkout master
